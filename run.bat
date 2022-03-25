d335852fc04296d60c699bd98af2596d4aa451db
polygraphene <polygraphene@gmail.com>
2022-03-26 00:50:41 +0900

    Reverse shell. A little safer.

--- run.bat
+++ run.bat
@@ -6,8 +6,7 @@ set adb=adb -d
 %adb% push dirtypipe-android startup-root magisk/busybox magisk/magiskpolicy %dir%
 %adb% shell chmod 755 %dir%/dirtypipe-android %dir%/startup-root %dir%/busybox %dir%/magiskpolicy
 %adb% shell %dir%/dirtypipe-android
-%adb% shell sleep 1
-%adb% shell %dir%/busybox telnet 127.0.0.1 10847
+%adb% shell /system/bin/nc -lp 10847
 
 pause
 
